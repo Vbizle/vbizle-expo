879de07b91968a5421767ba50f2564634ad3a89a
Vbizle <Vbizle.vb0@gmail.com>
2025-12-10 08:31:23 +0300

    Auto-sync: file changes

--- app/messages/[uid]/index.tsx
+++ app/messages/[uid]/index.tsx
@@ -1,4 +1,4 @@
-// TEST123
+// test auto push disabled
 import { useFocusEffect, useLocalSearchParams, useRouter } from "expo-router";
 import React, { useEffect, useRef, useState } from "react";
 import {
